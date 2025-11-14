603954cb1059d3b3acf18cafdebe95085c84e6b9
aaabrham <aaabrham421@gmail.com>
2025-11-15 02:51:32 +0300

    improve login action validation using zod, show proper error messages and use types on action

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -1,38 +1,84 @@
 import { Routes } from "@/data/routes.js";
 import prisma from "@/server/prisma.js";
 import { createSession } from "@/server/session.js";
-import { fail, redirect } from "@sveltejs/kit";
+import {
+  fail,
+  isHttpError,
+  isRedirect,
+  redirect,
+  type ActionFailure,
+  type Actions,
+} from "@sveltejs/kit";
 import bcrypt from "bcrypt";
+import z from "zod";
 
-export const actions = {
-  default: async ({ request, cookies }) => {
-    const data = await request.formData();
-    const email = data.get("email")?.toString();
-    const password = data.get("password")?.toString();
+const loginSchema = z.object({
+  email: z
+    .email("Please enter a valid email address")
+    .trim()
+    .min(1, "Email is required"),
+  password: z
+    .string({ error: "Password is required" })
+    .trim()
+    .min(6, "Password must be at least 6 characters long"),
+});
 
-    if (!email) {
-      return fail(400, { message: "Email is required" });
+export type LoginFormData = z.infer<typeof loginSchema>;
+
+export interface LoginFormFail {
+  message?: string;
+  errors?: Partial<Record<keyof LoginFormData, string[]>>;
+  values?: Partial<LoginFormData>;
+}
+
+export const actions: Actions = {
+  default: async ({
+    request,
+    cookies,
+  }): Promise<ActionFailure<LoginFormFail> | void> => {
+    try {
+      const data = await request.formData();
+      const formData = Object.fromEntries(data);
+
+      const parsed = loginSchema.safeParse(formData);
+
+      if (!parsed.success) {
+        const errors = z.flattenError(parsed.error);
+        return fail(400, {
+          message: "Invalid form submission",
+          errors: errors.fieldErrors,
+          values: formData,
+        });
+      }
+
+      const { email, password } = parsed.data;
+
+      // get user by email
+      const user = await prisma.user.findUnique({ where: { email } });
+      if (!user) {
+        return fail(400, { message: "Invalid credentials" });
+      }
+
+      // verify password
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) {
+        return fail(400, { message: "Invalid credentials" });
+      }
+
+      // create user session and set cookie
+      await createSession(user.id, cookies);
+
+      return redirect(302, Routes.home);
+    } catch (error) {
+      // let SvelteKit handle redirects & HTTP errors
+      if (isRedirect(error) || isHttpError(error)) {
+        throw error;
+      }
+
+      console.log("Login error: ", error);
+      return fail(500, {
+        message: "something went wrong, please try again later",
+      });
     }
-
-    if (!password) {
-      return fail(400, { message: "Password is required" });
-    }
-
-    // get user by email
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) {
-      return fail(400, { message: "Invalid credentials" });
-    }
-
-    // verify password
-    const valid = await bcrypt.compare(password, user.password);
-    if (!valid) {
-      return fail(400, { message: "Invalid credentials" });
-    }
-
-    // create user session and set cookie
-    await createSession(user.id, cookies);
-
-    throw redirect(302, Routes.home);
   },
 };
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -11,14 +11,16 @@
   import { Label } from "@/components/ui/label";
   import { Routes } from "@/data/routes";
   import type { PageProps } from "./$types";
+  import { enhance } from "$app/forms";
+  import { fade } from "svelte/transition";
 
-  const { data, form }: PageProps = $props();
+  const { form } = $props();
+
+  let pending = $state(false);
 </script>
 
-{form?.message}
-
 <div class="w-screen h-screen flex justify-center items-center">
-  <Card class="flex w-full max-w-sm flex-col gap-6">
+  <Card class="flex w-[90%] max-w-sm flex-col gap-6">
     <CardHeader class="text-center">
       <CardTitle class="text-xl">Welcome</CardTitle>
       <CardDescription
@@ -26,8 +28,27 @@
       >
     </CardHeader>
 
-    <form method="POST">
+    <form
+      method="POST"
+      use:enhance={() => {
+        pending = true;
+
+        return async ({ update }) => {
+          await update();
+          pending = false;
+        };
+      }}
+    >
       <CardContent class="grid gap-4">
+        {#if form?.message}
+          <div
+            class="p-3 text-sm rounded-md border bg-red-50 border-red-500 text-red-600 font-semibold"
+            transition:fade
+          >
+            {form.message}
+          </div>
+        {/if}
+
         <div class="grid gap-2">
           <Label for="email">email</Label>
           <Input
@@ -35,7 +56,11 @@
             name="email"
             type="email"
             placeholder="email@example.com"
+            class={form?.errors?.email ? "border-red-500" : ""}
           />
+          {#if form?.errors?.email}
+            <p class="text-xs text-red-500">{form.errors.email?.[0]}</p>
+          {/if}
         </div>
 
         <div class="grid gap-2">
@@ -48,9 +73,25 @@
               Forgot your password?
             </a> -->
           </div>
-          <Input id="password" name="password" type="password" />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            class={form?.errors?.password ? "border-red-500" : ""}
+          />
+          {#if form?.errors?.password}
+            <p class="text-xs text-red-500">
+              {form.errors.password?.[0]}
+            </p>
+          {/if}
         </div>
-        <Button type="submit" class="cursor-pointer">Login</Button>
+        <Button type="submit" class="cursor-pointer" disabled={pending}>
+          {#if pending}
+            <span class="animate-pulse">Logging in...</span>
+          {:else}
+            Login
+          {/if}
+        </Button>
 
         <div class="text-center m-auto inline-block text-sm">
           Don't have an account?
