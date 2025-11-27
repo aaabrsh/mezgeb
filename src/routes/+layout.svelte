9c4a208e937eed217810ac8af2955f8585212d74
aaabrham <aaabrham421@gmail.com>
2025-11-28 01:54:17 +0300

    add theme toggle button component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  import { ModeWatcher } from "mode-watcher";
   import "../app.css";
 
   let { children } = $props();
 </script>
 
+<ModeWatcher />
 <section class="h-screen w-screen overflow-auto bg-background">
   {@render children()}
 </section>
