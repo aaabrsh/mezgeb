4c707c6e05a0e78a131acb055cea274a188dfbe5
aaabrham <aaabrham421@gmail.com>
2025-11-30 00:14:10 +0300

    add raw/unstyled button variant, use it for theme toggle button

--- src/lib/components/ui/theme-toggle/theme-toggle.svelte
+++ src/lib/components/ui/theme-toggle/theme-toggle.svelte
@@ -6,7 +6,12 @@
   import { Button } from "$lib/components/ui/button/index.js";
 </script>
 
-<Button onclick={toggleMode} variant="outline" size="icon">
+<Button
+  variant="raw"
+  size="icon"
+  onclick={toggleMode}
+  class="text-primary dark:text-secondary"
+>
   <SunIcon
     class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
   />
