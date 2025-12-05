326055c44f133f09870c3a70fb89d7bccd4c6155
aaabrham <aaabrham421@gmail.com>
2025-12-05 18:55:09 +0300

    optionally show (optional) and (required) texts next to label

--- src/lib/components/ui/label/label.svelte
+++ src/lib/components/ui/label/label.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
-	import { Label as LabelPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+  import { Label as LabelPrimitive } from "bits-ui";
+  import { cn } from "$lib/utils.js";
 
-	let {
-		ref = $bindable(null),
-		class: className,
-		...restProps
-	}: LabelPrimitive.RootProps = $props();
+  type Props = LabelPrimitive.RootProps &
+    (
+      | { required?: boolean; optional?: undefined }
+      | { optional?: boolean; required?: undefined }
+    );
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    required,
+    optional,
+    children,
+    ...restProps
+  }: Props = $props();
 </script>
 
 <LabelPrimitive.Root
-	bind:ref
-	data-slot="label"
-	class={cn(
-		"flex select-none items-center gap-2 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
-		className
-	)}
-	{...restProps}
-/>
+  bind:ref
+  data-slot="label"
+  class={cn(
+    "flex select-none items-center gap-2 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
+    className
+  )}
+  {...restProps}
+>
+  {#if children}
+    {@render children()}
+  {/if}
+
+  {#if optional}
+    <div class="text-neutral-600">(optional)</div>
+  {/if}
+  {#if required}
+    <div class="text-neutral-600">(Required)</div>
+  {/if}
+</LabelPrimitive.Root>
