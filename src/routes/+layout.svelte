d5b1ee31cc155d51e7ae812cf19f17e15a31a731
aaabrham <aaabrham421@gmail.com>
2025-12-12 22:52:44 +0300

    addd sonner component for toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import { ModeWatcher } from "mode-watcher";
+  import { Toaster } from "$lib/components/ui/sonner/index.js";
   import "../app.css";
 
   let { children } = $props();
 </script>
 
 <ModeWatcher />
+<Toaster position="bottom-center" richColors />
 <section class="h-screen w-screen overflow-auto bg-background">
   {@render children()}
 </section>
