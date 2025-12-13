3ddee9befe50074c1b77b6393a2e9e5efcbeb513
aaabrham <aaabrham421@gmail.com>
2025-12-13 23:35:24 +0300

    update currency list style, show modal before deletion

--- src/lib/components/currency/currency-list.svelte
+++ src/lib/components/currency/currency-list.svelte
@@ -5,6 +5,14 @@
   import type { Currency } from "@prisma/client";
   import { enhance } from "$app/forms";
   import { cn } from "@/utils";
+  import {
+    Item,
+    ItemActions,
+    ItemContent,
+    ItemDescription,
+    ItemTitle,
+  } from "@/components/ui/item";
+  import { ConfirmationModal } from "@/components/ui/confirmation-modal";
 
   type Props = {
     currencies: Currency[];
@@ -14,67 +22,85 @@
 
   let { currencies, onEdit, onDeleteComplete }: Props = $props();
   let delete_pending: Record<string, boolean> = $state({});
+  let data_to_delete = $state<string | null>(null);
+  let deletion_form: HTMLFormElement;
 </script>
 
 <div class="space-y-3">
-  {#each currencies as currency}
-    <div class="flex gap-2 items-center">
-      <Card
-        class={cn("flex-grow p-0 shadow-none", { "bg-neutral-600/10": !currency.userId })}
-      >
-        <div class="flex gap-2 px-5 py-3 w-full justify-center">
-          <div class="flex-grow">
-            <div class="text-primary font-semibold">
-              {currency.abbrev}
-            </div>
-            <div class="text-neutral-600">
-              {currency.name || "-"}
-            </div>
+  {#each currencies as currency (currency.id)}
+    <Item variant={currency.userId ? "outline" : "muted"} class="wifull">
+      <ItemContent>
+        <ItemTitle class="text-primary">{currency.abbrev}</ItemTitle>
+        <ItemDescription class="text-neutral-600">
+          {currency.name || "-"}
+        </ItemDescription>
+      </ItemContent>
+      <ItemActions>
+        {#if currency.symbol}
+          <div
+            class="flex items-center justify-center font-semibold text-neutral-600 h-12 w-12 rounded-[50%] bg-background"
+          >
+            {currency.symbol}
           </div>
-          {#if currency.symbol}
-            <div
-              class="flex items-center justify-center font-semibold text-neutral-600 h-12 w-12 rounded-[50%] bg-background"
-            >
-              {currency.symbol}
-            </div>
-          {/if}
-        </div>
-      </Card>
+        {/if}
+      </ItemActions>
 
       {#if currency.userId}
-        <Button
-          variant="raw"
-          class="rounded-full h-10 w-10 text-primary hover:bg-primary/20 transition p-2"
-          onclick={() => onEdit && onEdit(currency)}
-        >
-          <Pencil size={16} />
-        </Button>
-        <form
-          method="POST"
-          action="?/delete"
-          use:enhance={() => {
-            delete_pending[currency.id] = true;
-            return async ({ update }) => {
-              await update();
-              delete_pending[currency.id] = false;
-              onDeleteComplete && onDeleteComplete();
-            };
-          }}
-        >
-          <input type="hidden" name="id" value={currency.id} />
+        <ItemActions class={"border-l-1 ps-4"}>
           <Button
-            type="submit"
+            variant="raw"
+            class="rounded-full h-10 w-10 text-primary hover:bg-primary/20 transition p-2"
+            onclick={() => onEdit && onEdit(currency)}
+          >
+            <Pencil size={16} />
+          </Button>
+
+          <Button
             variant="raw"
             class="rounded-full h-10 w-10 text-red-600 hover:bg-red-200 transition p-2"
             disabled={delete_pending[currency.id]}
             loading={delete_pending[currency.id]}
+            onclick={() => {
+              data_to_delete = currency.id;
+            }}
           >
             {#if !delete_pending[currency.id]}
               <Trash2 size={16} />
             {/if}
           </Button>
-        </form>
+        </ItemActions>
       {/if}
-    </div>
+    </Item>
   {/each}
 </div>
+
+<!-- Hidden Currency Delete form to be triggered programmatically -->
+<form
+  method="POST"
+  action="?/delete"
+  bind:this={deletion_form}
+  use:enhance={() => {
+    delete_pending[data_to_delete || ""] = true;
+    return async ({ update }) => {
+      await update();
+      delete_pending[data_to_delete || ""] = false;
+      data_to_delete = null;
+      onDeleteComplete && onDeleteComplete();
+    };
+  }}
+>
+  <input type="hidden" name="id" value={data_to_delete} />
+</form>
+
+<!-- Confirmation modal to be shown before deletion -->
+<ConfirmationModal
+  open={!!data_to_delete}
+  onConfirm={() => {
+    deletion_form?.requestSubmit();
+  }}
+  onClose={() => {
+    data_to_delete = null;
+  }}
+  title="Are you sure you want to delete this currency?"
+  loading={delete_pending[data_to_delete || ""]}
+/>
